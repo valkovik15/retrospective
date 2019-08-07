4fc7ca7c394cacdc6f354864768c8c713ca8b6c1
Iris <37576779+iris-the-fox@users.noreply.github.com>
2019-08-07 15:07:50 +0300

    GH-10: Board new form (#21)
    * GH-10: Board new form
    
    * Update yarn.lock
    
    * Add date with strftime to controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,24 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+
+  def new
+    @board = Board.new(title: Date.today.strftime('%d-%m-%Y'))
+  end
+
+  def create
+    @board = Board.new(board_params)
+    @board.creator_id = current_user.id
+    if @board.save
+      redirect_to @board, notice: 'Board was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title)
+  end
 end
