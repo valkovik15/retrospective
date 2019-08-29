bab67abb6bbf3ff4e9acebf876f3dcb085413de6
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-29 05:59:12 +0300

    improve json

--- app/javascript/board_spa/fake_json.js
+++ app/javascript/board_spa/fake_json.js
@@ -50,7 +50,106 @@ const jsonResponse = {
         "avatar_url": "https://i.pravatar.cc/100?img=40",
         "ready": false
       }
-    ]
+    ],
+//========================================================
+    "columns": [
+      {
+        "title": "MAD",
+        "cards": [
+          {
+            "body": "This is a mad card from member1",
+            "likes": 5,
+            "abilities": ["edit","like","delete"],
+            "author": {
+                        "uid": "",
+                        "email": "member1@mail.com",
+                        "avatar_url": "https://i.pravatar.cc/100?img=10"
+                      }
+          },
+          {
+            "body": "This is a mad card from member2",
+            "likes": 5,
+            "abilities": ["edit","like","delete"],
+            "author": {
+                        "uid": "",
+                        "email": "member2@mail.com",
+                        "avatar_url": "https://i.pravatar.cc/100?img=20"
+                      }
+          }
+        ]
+      },
+      {
+        "title": "SAD",
+        "cards": [
+          {
+            "body": "This is a sad card from member2",
+            "likes": 5,
+            "abilities": ["edit","like","delete"],
+            "author": {
+                        "uid": "",
+                        "email": "member2@mail.com",
+                        "avatar_url": "https://i.pravatar.cc/100?img=20"
+                      }
+          },
+          {
+            "body": "This is a sad card from member3",
+            "likes": 5,
+            "abilities": ["edit","like","delete"],
+            "author": {
+                        "uid": "",
+                        "email": "member3@mail.com",
+                        "avatar_url": "https://i.pravatar.cc/100?img=30"
+                      }
+          }
+        ]
+      },
+      {
+        "title": "GLAD",
+        "cards": [
+          {
+            "body": "This is a glad card from member3",
+            "likes": 5,
+            "abilities": ["edit","like","delete"],
+            "author": {
+                        "uid": "",
+                        "email": "member3@mail.com",
+                        "avatar_url": "https://i.pravatar.cc/100?img=30"
+                      }
+          },
+          {
+            "body": "This is a glad card from member4",
+            "likes": 5,
+            "abilities": ["edit","like","delete"],
+            "author": {
+                        "uid": "",
+                        "email": "member4@mail.com",
+                        "avatar_url": "https://i.pravatar.cc/100?img=40"
+                      }
+          }
+        ]
+      }    
+    ],
+//===========================================
+    "prev_action_items": [
+      {
+        "body": "prev action item 1",
+        "status": "done"
+      },
+      {
+        "body": "prev action item 2",
+        "status": "prolong"
+      }
+    ],
+//============================================
+    "action_items": [
+      {
+        "body": "action item 1"
+      },
+      {
+        "body": "action item 2"
+      }
+    ],
+ //===========================================
   }
 };
 
