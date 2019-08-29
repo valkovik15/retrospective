c164845eb07fcaa1eea048e7181b3dc7f53b6af0
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-29 12:21:53 +0300

    like bar done

--- app/javascript/board_spa/components/columns/cards/Card.jsx
+++ app/javascript/board_spa/components/columns/cards/Card.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 
 class Card extends React.Component {
   render() {
-    const { body, color, author } = this.props;
+    const { body, color, author, likes } = this.props;
 
 
     return (
       <div className='card' style={{background: color}}>
         <div className='card-body text'>{body}</div>
 
-        <div className='level' style={{marginTop: '10px'}}>
+        <div className='level like-bar'>
           <div className='level-left image is-32x32'>
             <img className='is-rounded' src={author.avatar_url}/> 
           </div>        
         
-        {/*
-          <span className="level-right icon">
-            <i className="fas fa-heart"></i>
-            <span style={{fontFamily: 'Helvetica'}}>5</span>
-          </span>
-        */}
+          <div className='level-right'>
+            <i className='fas fa-heart'></i>
+            <span id='like-num'>{likes}</span>
+          </div>
         </div>
       </div>
     )
