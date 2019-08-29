049b459841c8e0b479bae4ce2f6f08f89b6970ab
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-29 08:50:47 +0300

    Add avatars

--- app/javascript/board_spa/components/columns/Column.jsx
+++ app/javascript/board_spa/components/columns/Column.jsx
@@ -8,9 +8,9 @@ class Column extends React.Component {
 
     function matchColor(title) {
       switch(title) {
-        case 'MAD': return('hsl(348, 100%, 61%)');
-        case 'SAD': return('hsl(217, 71%, 53%)');
-        case 'GLAD': return('hsl(141, 71%, 48%)');
+        case 'MAD': return('hsla(348, 100%, 61%, 0.5)');
+        case 'SAD': return('hsla(217, 71%, 53%, 0.5)');
+        case 'GLAD': return('hsla(141, 71%, 48%, 0.5)');
       }
       //if (title == 'MAD') { return('hsl(348, 100%, 61%)'); }
       //if (title == 'SAD') { return('hsl(217, 71%, 53%)'); }
@@ -18,7 +18,8 @@ class Column extends React.Component {
     }
  
     const cards = this.props.cards.map(function(card) {
-      return(<Card body={card.body} abilities={card.abilities} likes={card.likes} author={card.author} color={matchColor(title)}/>);
+      return(<Card body={card.body} abilities={card.abilities} 
+             likes={card.likes} author={card.author} color={matchColor(title)}/>);
     });
 
     return (
--- app/javascript/board_spa/components/columns/cards/Card.jsx
+++ app/javascript/board_spa/components/columns/cards/Card.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 class Card extends React.Component {
   render() {
-    const { body, color } = this.props;
+    const { body, color, author } = this.props;
 
 
     return (
       <div className='card' style={{background: color}}>
-        <div className='card-body'>{body}</div>
+        <div className='card-body text'>{body}</div>
+        
+        <div className='image is-32x32'>
+          <img className='is-rounded' style={{marginTop: '10px'}} src={author.avatar_url}/> 
+        </div>        
+        
+        <span class="icon has-text-success">
+          <i class="fas fa-ban"></i>
+       </span>
       </div>
     )
   }
