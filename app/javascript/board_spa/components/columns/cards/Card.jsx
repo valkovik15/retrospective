255f0e114e7ce03fd37f7e0f5394caca12fce2a2
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-29 09:30:17 +0300

    add fontawesome icons

--- app/javascript/board_spa/components/columns/cards/Card.jsx
+++ app/javascript/board_spa/components/columns/cards/Card.jsx
@@ -8,14 +8,17 @@ class Card extends React.Component {
     return (
       <div className='card' style={{background: color}}>
         <div className='card-body text'>{body}</div>
+
+        <div className='level' style={{marginTop: '10px'}}>
+          <div className='level-left image is-32x32'>
+            <img className='is-rounded' src={author.avatar_url}/> 
+          </div>        
         
-        <div className='image is-32x32'>
-          <img className='is-rounded' style={{marginTop: '10px'}} src={author.avatar_url}/> 
-        </div>        
-        
-        <span class="icon has-text-success">
-          <i class="fas fa-ban"></i>
-       </span>
+          <span className="level-right icon">
+            <i className="fas fa-heart"></i>
+            <span style={{fontFamily: 'Helvetica'}}>5</span>
+          </span>
+        </div>
       </div>
     )
   }
