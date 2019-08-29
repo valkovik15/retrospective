5a36cd8a853453896b4bca5b4850ead152d0bd95
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-08-29 03:25:10 +0300

    Add buttonset component

--- app/javascript/board_spa/components/ButtonSet.jsx
+++ app/javascript/board_spa/components/ButtonSet.jsx
@@ -4,13 +4,21 @@ import { CurrentUserContext } from '../current_user_context'
 
 class ButtonSet extends React.Component {
   render() {
+
+    const { isMember, ready } = this.context;
+
+    let readyButtonClass = 'button';
+    if (ready) {
+      readyButtonClass += ' is-success';
+    }
+
     return (
       <div className='component lvl2'>
         <div className='text lable'>BUTTON-SET</div>
 
-        <button id='join'>JOIN</button>
-        <button id='leave'>LEAVE</button>
-        <button id='ready'>READY</button>
+        <button className='button' id='join' disabled={isMember}>JOIN</button>
+        <button className='button is-danger' id='leave' disabled={!isMember}>LEAVE</button>
+        <button className={readyButtonClass} id='ready' disabled={!isMember}>READY</button>
       </div>
     )
   }   
