9b32f7c4f574276aa38d5225a3639d1c59c525fc
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-10-02 14:48:52 +0300

    Improvement/GH-115: Rewrite specs for operations (#120)
    * rework continue operation specs
    
    * fix review comments

--- spec/operations/continue_spec.rb
+++ spec/operations/continue_spec.rb
@@ -3,26 +3,57 @@
 require 'rails_helper'
 
 RSpec.describe Boards::Continue do
-  let_it_be(:creator) { create(:user) }
-  let_it_be(:prev_board) { create(:board) }
-  let_it_be(:creatorship) { create(:membership, board: prev_board, user: creator, role: 'creator') }
+  include Dry::Monads[:result]
 
-  describe '#default_board_name' do
-    subject { described_class.new(prev_board, creator).send(:default_board_name) }
-
-    it { is_expected.to eq Date.today.strftime('%d-%m-%Y') }
+  let!(:prev_creator) { create(:user) }
+  let!(:current_user) { create(:user) }
+  let!(:prev_board) { create(:board) }
+  let!(:creatorship) do
+    create(:membership, board: prev_board, user: prev_creator, role: 'creator', ready: true)
+  end
+  let!(:membership) do
+    create(:membership, board: prev_board, user: current_user, role: 'member')
   end
 
-  describe '#prev_board_continued?' do
-    subject { described_class.new(prev_board, creator).send(:prev_board_continued?) }
+  subject { described_class.new(prev_board, current_user).call }
 
-    context 'when board was not previously continued' do
-      it { is_expected.to eq false }
+  context 'when prev_board was previously continued' do
+    before do
+      allow_any_instance_of(described_class)
+        .to receive(:prev_board_continued?)
+        .and_return(true)
     end
 
-    context 'when board was previously continued' do
-      let_it_be(:next_board) { create(:board, previous_board: prev_board) }
-      it { is_expected.to eq true }
+    it { is_expected.to be_a(Dry::Monads::Result::Failure) }
+    it 'fails with standard error' do
+      expect(subject.failure).to be_a(StandardError)
+      expect(subject.failure.message)
+        .to eq 'This board was already continued! Only one continuation per board is allowed!'
+    end
+  end
+
+  context 'when prev_board was not previously continued' do
+    before do
+      allow_any_instance_of(described_class)
+        .to receive(:prev_board_continued?)
+        .and_return(false)
+    end
+
+    it { is_expected.to be_a(Dry::Monads::Result::Success) }
+    it 'returns board in the result value' do
+      expect(subject.value!).to be_instance_of(Board)
+    end
+    it 'duplicates memberships from the previous board' do
+      expect { subject }.to change(Membership, :count).by(prev_board.memberships.count)
+    end
+    it 'assigns duplicated memberships to the new board' do
+      expect(subject.value!.users).to contain_exactly(*prev_board.users)
+    end
+    it 'sets dupicated memberships ready statuses to false' do
+      expect(subject.value!.memberships).not_to include(be_ready)
+    end
+    it 'sets current_user as the creator of the new board' do
+      expect(subject.value!.memberships.find_by(role: 'creator').user_id).to eq current_user.id
     end
   end
 end
