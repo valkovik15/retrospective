e5970f0a067657e7c4bf6672b2fd3ecfd665b190
Vitalii Pecherin <vitalii.pecherin@cybergizer.com>
2019-10-12 20:02:22 +0300

    gitignore .env.*

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_112227) do
+ActiveRecord::Schema.define(version: 2019_09_26_082403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
