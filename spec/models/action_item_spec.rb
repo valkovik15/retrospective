a5ca4e6f67f8b595a5d58ce5ea3f0cdadc0c1dfc
nikolaiprivalov <52496677+nikolaiprivalov@users.noreply.github.com>
2019-10-18 13:50:38 +0300

    add aasm specs

--- spec/models/action_item_spec.rb
+++ spec/models/action_item_spec.rb
@@ -24,4 +24,51 @@ RSpec.describe ActionItem, type: :model do
       expect(action_item).to respond_to(:board)
     end
   end
+
+  describe 'aasm' do
+    subject { action_item }
+
+    context 'status: pending' do
+      let(:action_item) { build_stubbed(:action_item, status: 'pending') }
+
+      it { is_expected.to have_state(:pending) }
+
+      context 'allowed transitions' do
+        it { is_expected.to allow_transition_to(:closed) }
+        it { is_expected.to allow_transition_to(:done) }
+        it { is_expected.to_not allow_transition_to(:pending) }
+      end
+
+      it { is_expected.to transition_from(:pending).to(:closed).on_event(:close) }
+      it { is_expected.to transition_from(:pending).to(:done).on_event(:complete) }
+    end
+
+    context 'status: closed' do
+      let(:action_item) { build_stubbed(:action_item, status: 'closed') }
+
+      it { is_expected.to have_state(:closed) }
+
+      context 'allowed transitions' do
+        it { is_expected.to_not allow_transition_to(:closed) }
+        it { is_expected.to_not allow_transition_to(:done) }
+        it { is_expected.to allow_transition_to(:pending) }
+      end
+
+      it { is_expected.to transition_from(:closed).to(:pending).on_event(:reopen) }
+    end
+
+    context 'status: done' do
+      let(:action_item) { build_stubbed(:action_item, status: 'done') }
+
+      it { is_expected.to have_state(:done) }
+
+      context 'allowed transitions' do
+        it { is_expected.to_not allow_transition_to(:closed) }
+        it { is_expected.to_not allow_transition_to(:done) }
+        it { is_expected.to allow_transition_to(:pending) }
+      end
+
+      it { is_expected.to transition_from(:done).to(:pending).on_event(:reopen) }
+    end
+  end
 end
