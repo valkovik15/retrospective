34762cbe0218a3e5e0d3b1cb9d94700c6141f545
Valentin Kopotchel <valentin.kopothcel@cybergizer.com>
2020-05-22 10:59:36 +0300

    Fix sockets https bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://retrospective-rubizza.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = [ /https:\/\/retrospective-rubizza.*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
