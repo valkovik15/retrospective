f7299bcba45e5fbf899a68756e75ff162f4db8e1
Valentin Kopotchel <valentin.kopothcel@cybergizer.com>
2020-06-09 08:54:23 +0300

    Refactor CommentsDropdown component

--- app/javascript/components/Card/CommentsDropdown/index.js
+++ app/javascript/components/Card/CommentsDropdown/index.js
@@ -1,46 +1,43 @@
 import React, {useRef, useState} from 'react';
-
 import Picker from 'emoji-picker-react';
-
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSmile} from '@fortawesome/free-regular-svg-icons';
 
 const CommentsDropdown = props => {
-  const inputEl = useRef(null);
   const controlEl = useRef(null);
-
   const {visible, id, comments} = props;
-
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [newComment, setNewComment] = useState('');
 
-  const handleSubmit = () => {
-    controlEl.current.disabled = true;
-    fetch(`/api/${window.location.pathname}/cards/${id}/comments`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document
-          .querySelector("meta[name='csrf-token']")
-          .getAttribute('content')
-      },
-      body: JSON.stringify({
-        content: inputEl.current.value
-      })
-    })
-      .then(result => {
-        controlEl.current.disabled = false;
-        if (result.status === 200) {
-          inputEl.current.value = '';
-        } else {
-          throw result;
+  const handleSubmit = async commentContent => {
+    try {
+      controlEl.current.disabled = true;
+      const response = await fetch(
+        `/api/${window.location.pathname}/cards/${id}/comments`,
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': document
+              .querySelector("meta[name='csrf-token']")
+              .getAttribute('content')
+          },
+          body: JSON.stringify({
+            content: commentContent
+          })
         }
-      })
-      .catch(error => {
-        error.json().then(errorHash => {
-          console.log(errorHash.error);
-        });
-      });
+      );
+      controlEl.current.disabled = false;
+      if (response.status === 200) {
+        setNewComment('');
+      } else {
+        throw response;
+      }
+    } catch (error) {
+      const errorHash = await error.json();
+      console.log(errorHash.error);
+    }
   };
 
   const handleSmileClick = () => {
@@ -48,54 +45,60 @@ const CommentsDropdown = props => {
   };
 
   const handleEmojiPickerClick = (_, emoji) => {
-    inputEl.current.value += emoji.emoji;
+    setNewComment(comment => `${comment}${emoji.emoji}`);
   };
 
+  if (!visible) {
+    return null;
+  }
+
   return (
-    visible && (
-      <div className="column comments-column">
-        <div className="dropdown-comments-menu" role="menu">
-          <div className="dropdown-comments-content">
-            <div className="textarea-container dropdown-item">
-              <textarea ref={inputEl} className="textarea" />
-              <button
-                ref={controlEl}
-                className="button is-small"
-                type="button"
-                onClick={handleSubmit}
-              >
-                Add comment
-              </button>
-              <a className="has-text-info" onClick={handleSmileClick}>
-                <FontAwesomeIcon icon={faSmile} />
-              </a>
-            </div>
-            {showEmojiPicker && (
-              <Picker
-                style={{width: 'auto'}}
-                onEmojiClick={handleEmojiPickerClick}
-              />
-            )}
-            {comments.map(comment => {
-              return (
-                <div key={comment.id} className="dropdown-item">
-                  <div className="columns">
-                    <div className="column">{comment.content}</div>
-                    <div className="column is-two-fifths bottom-content">
-                      <img
-                        src={comment.author.avatar.thumb.url}
-                        className="avatar"
-                      />
-                      <span> by {comment.author.email.split('@')[0]}</span>
-                    </div>
+    <div className="column comments-column">
+      <div className="dropdown-comments-menu" role="menu">
+        <div className="dropdown-comments-content">
+          <div className="textarea-container dropdown-item">
+            <textarea
+              className="textarea"
+              value={newComment}
+              onChange={e => setNewComment(e.target.value)}
+            />
+            <button
+              ref={controlEl}
+              className="button is-small"
+              type="button"
+              onClick={() => handleSubmit(newComment)}
+            >
+              Add comment
+            </button>
+            <a className="has-text-info" onClick={handleSmileClick}>
+              <FontAwesomeIcon icon={faSmile} />
+            </a>
+          </div>
+          {showEmojiPicker && (
+            <Picker
+              style={{width: 'auto'}}
+              onEmojiClick={handleEmojiPickerClick}
+            />
+          )}
+          {comments.map(comment => {
+            return (
+              <div key={comment.id} className="dropdown-item">
+                <div className="columns">
+                  <div className="column">{comment.content}</div>
+                  <div className="column is-two-fifths bottom-content">
+                    <img
+                      src={comment.author.avatar.thumb.url}
+                      className="avatar"
+                    />
+                    <span> by {comment.author.email.split('@')[0]}</span>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    )
+    </div>
   );
 };
 
