548aa9acced2aca3d6d16d84ba2728b9bf85de91
valkovik15 <41728508+valkovik15@users.noreply.github.com>
2020-06-16 10:54:16 +0300

    Remove 500 error on visiting desk link, custom error handling (#35)
    * Fix board link open bug
    
    * Change 404 and 500 error pages
    
    Co-authored-by: Valentin Kopotchel <valentin.kopothcel@cybergizer.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_raven_context
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!, except: %i[sign_in sign_up]
   authorize :user, through: :current_or_guest_user
   verify_authorized unless: [:devise_controller?]
@@ -15,5 +16,19 @@ class ApplicationController < ActionController::Base
 
   def current_or_guest_user
     current_user || User.new
+  end
+
+  # Its important that the location is NOT stored if:
+  # - The request method is not GET (non idempotent)
+  # - The request is handled by a Devise controller such as Devise::SessionsController
+  # as that could cause an infinite redirect loop.
+  # - The request is an Ajax request as this can lead to very unexpected behaviour.
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
   end
 end
