840caa25f1b07269e6f371220285b28d295d0acb
valkovik15 <41728508+valkovik15@users.noreply.github.com>
2020-06-22 09:21:13 +0300

    Fix 404 page styling (#40)
    Co-authored-by: Valentin Kopotchel <valentin.kopothcel@cybergizer.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,6 @@ Rails.application.configure do
   config.action_cable.url = "ws://localhost:3000/cable"
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.public_file_server.enabled = true
 end
